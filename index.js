39ced867493c121885dccb75add6d8a0096273b8
Md. Abir Hasan Abir <mdabirhasan551@gmail.com>
2022-07-19 16:35:29 +0600

    elint added

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-const express= require('express');
-const app=express();
+const express = require('express')
+const app = express()
 
-app.get('/',function(req,res){
-
-   return res.send("Hello World");
+app.get('/', function (req, res) {
+  return res.send('Hello')
 })
 
-app.listen(8080,function(){
-    console.log("Server running at port 8080");
-});
-
+app.listen(8080, function () {
+  console.log('Server running at port 8080')
+})
